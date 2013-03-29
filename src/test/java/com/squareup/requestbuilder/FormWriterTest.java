19b3e611ae85547e86e2ba40045af81566559db1
Jake Wharton <jw@squareup.com>
2013-03-29 09:34:11 -0700

    Introduce initial API for multipart and form encoding.

---
+++ src/test/java/com/squareup/requestbuilder/FormWriterTest.java
@@ -0,0 +1,44 @@
+// Copyright 2013 Square, Inc.
+package com.squareup.requestbuilder;
+
+import java.io.ByteArrayOutputStream;
+import org.junit.Test;
+
+import static com.squareup.requestbuilder.Utils.UTF_8;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class FormWriterTest {
+  @Test public void urlEncoding() throws Exception {
+    FormEncoding fe = new FormEncoding.Builder() //
+        .addPart("a&b", "c=d") //
+        .addPart("space, the", "final frontier") //
+        .build();
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    fe.writeBodyTo(out);
+    String actual = new String(out.toByteArray(), UTF_8);
+    assertThat(actual).isEqualTo("a%26b=c%3Dd&space%2C+the=final+frontier");
+  }
+
+  @Test public void encodedPairs() throws Exception {
+    FormEncoding fe1 = new FormEncoding.Builder() //
+        .addPart("sim", "ple") //
+        .build();
+
+    ByteArrayOutputStream out1 = new ByteArrayOutputStream();
+    fe1.writeBodyTo(out1);
+    String actual1 = new String(out1.toByteArray(), UTF_8);
+    assertThat(actual1).isEqualTo("sim=ple");
+
+    FormEncoding fe2 = new FormEncoding.Builder() //
+        .addPart("sim", "ple") //
+        .addPart("hey", "there") //
+        .addPart("help", "me") //
+        .build();
+
+    ByteArrayOutputStream out2 = new ByteArrayOutputStream();
+    fe2.writeBodyTo(out2);
+    String actual2 = new String(out2.toByteArray(), UTF_8);
+    assertThat(actual2).isEqualTo("sim=ple&hey=there&help=me");
+  }
+}
---
+++ src/test/java/com/squareup/requestbuilder/MultipartWriterTest.java
@@ -0,0 +1,59 @@
+// Copyright 2013 Square, Inc.
+package com.squareup.requestbuilder;
+
+import java.io.ByteArrayOutputStream;
+import org.junit.Test;
+
+import static com.squareup.requestbuilder.Utils.UTF_8;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class MultipartWriterTest {
+  @Test(expected = IllegalStateException.class)
+  public void onePartRequired() throws Exception {
+    new Multipart.Builder().build();
+  }
+
+  @Test public void singlePart() throws Exception {
+    String expected = "" //
+        + "--123\r\n" //
+        + "\r\n" //
+        + "Hello, World!\r\n" //
+        + "--123--";
+
+    Multipart m = new Multipart.Builder("123")
+        .addPart(new TestPart("Hello, World!"))
+        .build();
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    m.writeBodyTo(out);
+    String actual = new String(out.toByteArray(), UTF_8);
+    assertThat(actual).isEqualTo(expected);
+    assertThat(m.getHeaders()).containsExactly("Content-Type: multipart/mixed; boundary=123");
+  }
+
+  @Test public void threeParts() throws Exception {
+    String expected = ""
+        + "--123\r\n"
+        + "\r\n"
+        + "Quick\r\n"
+        + "--123\r\n"
+        + "\r\n"
+        + "Brown\r\n"
+        + "--123\r\n"
+        + "\r\n"
+        + "Fox\r\n"
+        + "--123--";
+
+    Multipart m = new Multipart.Builder("123")
+        .addPart(new TestPart("Quick"))
+        .addPart(new TestPart("Brown"))
+        .addPart(new TestPart("Fox"))
+        .build();
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    m.writeBodyTo(out);
+    String actual = new String(out.toByteArray(), UTF_8);
+    assertThat(actual).isEqualTo(expected);
+    assertThat(m.getHeaders()).containsExactly("Content-Type: multipart/mixed; boundary=123");
+  }
+}
